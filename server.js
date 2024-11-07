e90418dbe977c8ab0bee52b062aad992da8f51a4
anitha1204 <selvam12042003@gmail.com>
2024-11-07 11:55:26 +0530

    main

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const guestRoutes = require('./routes/guestRoutes');
+const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
@@ -18,7 +20,12 @@ app.use(cors());
 
 // Define Routes
 app.use('/api/auth', authRoutes);
+app.use('/api', guestRoutes);
 
+
+
+// Error handling middleware
+app.use(errorHandler);
 
 const PORT = 1000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
