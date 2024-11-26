37b7eb16274698022fd7db957cf7a4f0f2a49ce0
anitha1204 <selvam12042003@gmail.com>
2024-11-26 12:56:57 +0530

    main

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,7 +4,8 @@ const bookingController = require('../controllers/bookingController');
 
 // Corrected routes without double slashes
 router.post('/', bookingController.addBooking); 
-router.get('/', bookingController.getBookings); 
+router.get('/', bookingController.getBookings);
+router.get('/search', bookingController.searchBookings); 
 router.get('/:id', bookingController.getBookingById); // Single slash for ':id'
 router.put('/:id', bookingController.updateBooking); // Single slash for ':id'
 router.delete('/:id', bookingController.deleteBooking); // Single slash for ':id'
